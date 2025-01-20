a522813cf0579f533311f0ca9914fd4a38dce6e7
Nattapan Meepiean <formulae445@gmail.com>
2025-01-20 13:18:45 +0700

    init project

---
+++ gout-backend/gout-backend/src/test/java/test/gout_backend/GoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package test.gout_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
